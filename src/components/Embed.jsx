5f6183b50d81cbaad26d8bb2de8d0f0a3428303c
Zack <schaxmann@gmail.com>
2022-09-25 19:23:27 +0100

    Embed scroll percentage

--- src/components/Embed.jsx
+++ src/components/Embed.jsx
@@ -1,19 +1,25 @@
 import "../styling/Splash.css";
+import React from "react";
+import { useScrollPercentage } from "react-scroll-percentage";
 
 function Embed() {
+  const [ref, percentage] = useScrollPercentage();
   return (
     <div className="embed">
-      <embed
-        src="https://standforukraine.com/"
-        width={1000}
-        height={500}
-      ></embed>
+      <p> Scrolled {percentage}</p>
+      <embed ref={ref} src="https://standforukraine.com/"></embed>
       {/* <embed src="https://riseofukraine.com/" width={1000} height={500}></embed>
       <embed
         src="https://supportukrainenow.org/"
         width={1000}
         height={500}
       ></embed> */}
+
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://standforukraine.com/"
+      ></iframe>
     </div>
   );
 }
