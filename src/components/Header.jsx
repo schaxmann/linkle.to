8c3a31119a3f9e4a85651b1831c1988e857726e2
Zack <schaxmann@gmail.com>
2022-09-25 18:13:39 +0100

    Additional tagline brainstorming

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,14 @@ function Header() {
         bringing <span className="bold">order</span> to the{" "}
         <span className="bold">chaos</span>
       </h2>
+      <h2>
+        <span className="bold">create</span> &{" "}
+        <span className="bold">share</span> internet playlists
+      </h2>
+      <h2>
+        share <span className="bold">internet playlists</span> with anyone{" "}
+        <span className="bold">completely free</span>
+      </h2>
       {/* <h2>internet playlists</h2> */}
     </header>
   );
